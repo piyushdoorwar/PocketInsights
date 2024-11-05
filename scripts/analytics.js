9537961a2923918f0076e6c9166e2c84054bfdef
Piyush Doorwar <piyushdoorwar@gmail.com>
2024-11-05 17:23:38 +0530

    Update analytics.js

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,9 +1,17 @@
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9KBTC02Z34"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// Dynamically load the Google Analytics library
+(function() {
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-9KBTC02Z34";
+  document.head.appendChild(script);
 
-  gtag('config', 'G-9KBTC02Z34');
-</script>
+  // Initialize Google Analytics after the library loads
+  script.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-9KBTC02Z34');
+  };
+})();
